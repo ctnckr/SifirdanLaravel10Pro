4b2528ab9ff76e9b3df4e59de7d6cd796d694ed8
ctnckr <cetinceker@arkingroup.com>
2024-11-04 18:10:04 +0200

    web.php denemeler

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hakkimizda', function () {
+    echo "Yapım Aşamasındadır...";
+});
+
+Route::get('/iletisim', function () {
+    echo "3Teker ;)";
+});
